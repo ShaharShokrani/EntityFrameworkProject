a8d4ad21accf16107371c2ee5b8759b6f03ca006
Shahar Shokrani <shokrano@gmail.com>
2018-11-24 21:53:06 +0200

    first commit

---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoDbContext();
+            var courses = context.GetCourses();
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            //var context = new DatabaseFirstDemoEntities();
+            //var post = new Post()
+            //{
+            //    Body = "Body",
+            //    DatePublished = DateTime.UtcNow,
+            //    Title = "Title",
+            //    PostID = 1
+            //};
+
+            //context.Posts.Add(post);
+            //context.SaveChanges();
+        }
+    }
+}
