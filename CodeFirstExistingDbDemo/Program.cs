a0608b641b23b9839aac63e95af321d65d01839b
Shahar Shokrani <shokrano@gmail.com>
2018-11-25 13:21:13 +0200

    Initial model

---
+++ CodeFirstExistingDbDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExistingDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
