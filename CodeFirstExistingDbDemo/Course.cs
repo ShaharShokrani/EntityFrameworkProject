361a007bb30db305920901bf521571427d3bda2d
Shahar Shokrani <shokrano@gmail.com>
2018-11-25 13:41:02 +0200

    Add Category to Course

--- CodeFirstExistingDbDemo/Course.cs
+++ CodeFirstExistingDbDemo/Course.cs
@@ -21,6 +21,8 @@ namespace CodeFirstExistingDbDemo
 
         public string Description { get; set; }
 
+        public Category Category { get; set; }
+
         public int Level { get; set; }
 
         public short FullPrice { get; set; }
